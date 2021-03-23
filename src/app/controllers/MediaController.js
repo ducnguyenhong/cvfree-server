04ffae0941afc37c71741012e712cf4a21c33011
Nguyen Duc <ducnh1@egroup.vn>
2021-03-23 22:16:36 +0700

    Sửa lại api upload trả về đúng url

--- src/app/controllers/MediaController.js
+++ src/app/controllers/MediaController.js
@@ -2,10 +2,10 @@ const jsonRes = require('../helper/json-response')
 const CONSTANTS = require('../../constants')
 
 class UserController {
-  
+
   // [POST] /media/upload
   async upload(req, res, next) {
-    const staticURL = `${req.file.path}`.replace('src','')
+    const staticURL = `${req.file.path}`.slice(10, req.file.path.length)
     res.status(200).json(jsonRes.success(200, {
       url: `${CONSTANTS.serverURL}${staticURL}`
     }, 'UPLOAD_SUCCESS'))
--- src/routes/media.js
+++ src/routes/media.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const multer = require('multer')
-const upload = multer({dest: './src/media-upload/cv/avatar'})
+const upload = multer({dest: './src/public/uploads/cv/avatar'})
 const router = express.Router();
 const mediaController = require('../app/controllers/MediaController');
 const authMDW = require('../app/helper/middle-ware-auth')
